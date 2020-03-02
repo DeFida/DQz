f0e0e4ef1a8daf4d40df41abfaafa9051800c1ae
DeFida <57841128+DeFida@users.noreply.github.com>
2020-03-02 23:17:36 +0500

    df

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ screen = pygame.display.set_mode(size)
 running = True
 pygame.display.set_caption('Angry py')
 
-#load image function
+#functions
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
     image = pygame.image.load(fullname).convert()
@@ -34,6 +34,17 @@ def is_col(the_first, the_second):
     else:
         return False
 
+def score_text(score):
+    score = score
+    font = pygame.font.Font(None, 30)
+    text = font.render('Score: ' + str(int(score)), 1, (250, 250, 0))
+    screen.blit(text, (60, 30))
+
+def speed_text(speed):
+    speed = speed
+    font = pygame.font.Font(None, 30)
+    text = font.render('Speed: ' + str(int(float(str(speed)[0:4]) * 100)), 1, (250, 250, 0))
+    screen.blit(text, (60, 60))
 
 #all aprites
 
@@ -41,7 +52,7 @@ back = load_image('background.png', (255, 255, 255))
 
 
 #pos of enemy
-poses = [235, 280, 325]
+poses = [235, 280, 325, 300, 295, 260, 270]
 
 class enemy():
     def __init__(self):
@@ -56,6 +67,20 @@ class enemy():
 
     def get_chara(self):
         return (self.x, self.y + 20, self.x + 20, self.y + 20)
+
+class enemy2():
+    def __init__(self):
+        self.x = int()
+        self.y = 330
+        self.enemy = load_image('enemy2.png', -1)
+
+    def render(self, x, y):
+        self.x = x
+        self.y = y
+        screen.blit(self.enemy, (self.x, self.y))
+
+    def get_chara(self):
+        return (self.x, self.y + 30, self.x + 30, self.y + 30)
 
 
 class pyPlayer():
@@ -75,11 +100,21 @@ class pyPlayer():
 
 player = pyPlayer()
 eny = enemy()
+eny2 = enemy2()
 
 x_of_enemy = random.choice(poses)
 y_of_enemy = 0
 
-velo = 0.5
+x_of_enemy2 = random.choice(poses)
+y_of_enemy2 = -600
+
+velo = 0.8
+
+clock = pygame.time.Clock()
+
+n_su = 1
+
+score = 0
 
 x = 280
 y = 330
@@ -96,24 +131,40 @@ while running:
     if pressed[pygame.K_LEFT]:
         x -= velo * 0.4
 
+    velo += clock.tick() / 80500
+    score += velo * 0.1
+
     y_of_enemy += velo
+    y_of_enemy2 += velo
 
     if y_of_enemy >= 400:
         y_of_enemy = 0
         x_of_enemy = random.choice(poses)
     x_of_enemy = x_of_enemy
 
+    if y_of_enemy2 >= 400:
+        y_of_enemy2 = 0
+        x_of_enemy2 = random.choice(poses)
+    x_of_enemy2 = x_of_enemy2
+
     if x <= 232:
         x = 232
     elif x >= 338:
         x = 338
 
-    if is_col(eny, player):
-        running = False
+    if is_col(eny, player) or is_col(eny2, player) or is_col(player, eny2):
+        velo = 0
+
+
     # update anau mnau
     screen.blit(back, (0, 0))
     player.render(x, y)
     eny.render(x_of_enemy, y_of_enemy)
-    pygame.display.flip()
+    eny2.render(x_of_enemy2, y_of_enemy2)
+    score_text(score)
+    speed_text(velo)
 
+    pygame.display.flip()
+print(score)
+print(velo)
 pygame.quit()
